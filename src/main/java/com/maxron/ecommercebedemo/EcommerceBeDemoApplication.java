91d686167f638c0ecab44987d38dd3d58314e07d
Wei <zero.maxima@gmail.com>
2026-01-20 15:26:41 +0800

    init: initial project

---
+++ src/main/java/com/maxron/ecommercebedemo/EcommerceBeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.maxron.ecommercebedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBeDemoApplication.class, args);
+    }
+
+}

66558eebcd5ab00c5f8e1680b9c6597240fafea3
Wei <zero.maxima@gmail.com>
2026-01-20 15:26:41 +0800

    init: initial project

---
+++ src/test/java/com/maxron/ecommercebedemo/EcommerceBeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxron.ecommercebedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
